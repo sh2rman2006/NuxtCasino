d2f3615fd023d75d0cddbc38962a2e21cde7f328
sh2rman2006 <153737385+sh2rman2006@users.noreply.github.com>
2024-12-06 01:16:54 +0300

    ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  ssr: true,
   modules: ["@nuxt/image", "@vueuse/nuxt", "@nuxt/icon"],
   css: ["bootstrap/dist/css/bootstrap.min.css"],
 });
